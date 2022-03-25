21aeacbe57b8873b1fed4bf7ea0a1b22c30d316a
Reiner Hüchting <reiner.huechting@gmx.de>
2022-03-25 13:22:28 +0100

    Klasse BinaryTree hinzugefügt.

---
+++ BinaryTree.java
@@ -0,0 +1,16 @@
+class BinaryTree {
+    
+    public BinaryTree() {
+        // TODO
+    }
+
+    public boolean isEmpty() {
+        // TODO
+        return true;
+    }
+
+    public String inOrderString() {
+        // TODO
+        return "";
+    }
+}
